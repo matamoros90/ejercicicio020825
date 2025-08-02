687a1f6f924fcc4c2807d659a97dda31f6bb8dba
Marlon Matamoros <matamorosmarlon1@gmail.com>
2025-08-02 17:24:23 -0600

    feat: descripción clara

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let sueldo=0,aumento,nuevoSueldo,acum=0,i=0; noValidos=0;
+let sueldo=0,aumento,nuevoSueldo,acum=0,i=0;noValidos=0;
 
 while(sueldo!=-1){
     sueldo=Number(prompt("Ingrese un sueldo"));
@@ -7,13 +7,13 @@ if((sueldo>=0) && (sueldo<1000)){
     nuevoSueldo=sueldo+aumento;
     acum=acum+nuevoSueldo;
     i++;
-    alert("El aumento es del 15% Q"+aumento+" el nuevo sueldo es Q"+nuevoSueldo)
+    alert("El aumento es del 15% Q"+aumento+"el nuevo sueldo es Q"+nuevoSueldo)
 }else if(sueldo>=1000){
     aumento=sueldo*0.12;
     nuevoSueldo=sueldo+aumento;
     acum=acum+nuevoSueldo;
     i++;
-    alert("El aumento es del 12% Q"+aumento+" el nuevo sueldo es Q"+nuevoSueldo)
+    alert("El aumento es del 12% Q"+aumento+"el nuevo sueldo es Q"+nuevoSueldo)
 }else{
     if(sueldo===-1){
         alert("Orale");
